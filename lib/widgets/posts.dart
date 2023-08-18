0f0dff7cbef3d740c69f542545d774cd4cec6015
Ckm54 <mutaic84@gmail.com>
2023-08-18 13:35:22 +0300

    :lipstick: update posts page ui

--- lib/widgets/posts.dart
+++ lib/widgets/posts.dart
@@ -9,20 +9,107 @@ class Posts extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
+      physics: const NeverScrollableScrollPhysics(),
       itemCount: posts.length,
       scrollDirection: Axis.vertical,
       shrinkWrap: true,
       itemBuilder: (context, index) {
         final post = posts[index];
-        return Container(
-          color: Colors.grey.shade400,
-          width: double.maxFinite,
-          child: Column(
-            children: [
-              // Expanded(flex: 3, child: Text(post.title)),
-              Text(post.title)
-            ],
-          ),
+        final imageUrl = posts[index].imageSmall;
+        final datePosted = DateTime.parse(posts[index].createdAt);
+
+        // If imageUrl is null, display a local image; otherwise, display the API image
+        final imageWidget = imageUrl == null
+            ? Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/default.jpg'),
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(8.0),
+                  ),
+                ),
+              ) // Replace with your local image path
+            : Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage('https://www.thebrand.ai/i/$imageUrl'),
+                  ),
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(8.0),
+                  ),
+                ),
+              );
+
+        return Column(
+          children: [
+            SizedBox(
+              width: double.maxFinite,
+              height: 100,
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: imageWidget,
+                  ),
+                  const SizedBox(
+                    width: 8.0,
+                  ),
+                  Expanded(
+                    flex: 3,
+                    child: Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            post.title,
+                            maxLines: 1,
+                            softWrap: false,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                "Category",
+                                style: TextStyle(
+                                    fontStyle: FontStyle.italic,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                              Text(post.categoryName),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              const Text("Posted:"),
+                              Text(
+                                  "${datePosted.day}-${datePosted.month}-${datePosted.year}")
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Divider(
+              height: 1,
+              color: Colors.grey,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+          ],
         );
       },
     );
