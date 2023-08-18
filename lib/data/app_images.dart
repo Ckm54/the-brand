dc6188b7a185c489a8f36c53229e42db7c6a9f31
Ckm54 <mutaic84@gmail.com>
2023-08-18 13:35:06 +0300

    preload images

--- lib/data/app_images.dart
+++ lib/data/app_images.dart
@@ -1,4 +1,5 @@
 class AppImages {
   static const String imagesPath = "assets/images/";
-  static const String coverImage = imagesPath + "cover.jpg";
+  static const String coverImage = "${imagesPath}cover.jpg";
+  static const String defaultImage = "${imagesPath}default.jpg";
 }
